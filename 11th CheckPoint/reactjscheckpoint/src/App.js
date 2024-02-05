20e4ef8c2bd651185e7c7aa51515dc6df5119f6d
Mohamed KRICHENE <87141255+KrichenMed@users.noreply.github.com>
2024-02-05 12:52:45 +0100

    Uploading via VSCode

--- 11th CheckPoint/reactjscheckpoint/src/App.js
+++ 11th CheckPoint/reactjscheckpoint/src/App.js
@@ -1,7 +1,38 @@
 import "./App.css";
+import CardExample from "./Components/Cards";
+import PlaintextExample from "./Components/Forms";
+import BasicExample from "./Components/Navb";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      {/* This is the navbar component */}
+      <BasicExample />
+      {/*This is the Form */}
+      <div
+        style={{
+          backgroundColor: "#F8F9FA",
+          margin: "10px 6% 10px 6%",
+          padding: "10px ",
+          borderRadius: "0.5em",
+        }}
+      >
+        <PlaintextExample />
+      </div>
+      {/*These are the Cards */}
+      <div
+        style={{
+          backgroundColor: "#F8F9FA",
+          margin: "10px 6% 10px 6%",
+          padding: "10px ",
+          borderRadius: "0.5em",
+        }}
+      >
+        <CardExample />
+      </div>
+    </div>
+  );
 }
 
 export default App;
